c9b92dae445590459dd6719a56f6a0f400f9218e
Emmanuel <emmanuelmelgoza1@gmail.com>
2025-08-19 19:12:53 -0600

    Commented out follows in the seeder init file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -32,14 +32,14 @@ def seed():
         undo_wishlists()
         undo_listingcategories()
         undo_listingconditions()
-        undo_follows()
+        # undo_follows()
         undo_listings()
         undo_users()
 
 
     # Add other seed functions here
     seed_users()
-    seed_follows()
+    # seed_follows()
     seed_listings()
     seed_listingimages()
     seed_comments()
@@ -60,6 +60,6 @@ def undo():
     undo_wishlists()
     undo_listingcategories()
     undo_listingconditions()
-    undo_follows()
+    # undo_follows()
     undo_listings()
     undo_users()
